f9cedebce12dda505c9b7013dad48d2cf19aec62
Muhammad Hassan <immaharhasaan@gmail.com>
2025-11-30 10:43:39 +0500

    your message

---
+++ lib/config/app_config.dart
@@ -0,0 +1,6 @@
+class AppConfig {
+  // Backend ka base URL
+  // Agar backend local me run ho raha hai, use "http://127.0.0.1:8000"
+  // Agar emulator / mobile device, use LAN IP: "http://192.168.x.x:8000"
+  static const String baseUrl = "http://127.0.0.1:8000";
+}
